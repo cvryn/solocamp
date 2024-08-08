92b9b16a1c74660e6c40fa20ef3876af48e7b11a
caryn <ca.ryn.ono.w@gmail.com>
2024-08-08 10:54:56 -0700

    starter

--- pre-project-practice/app/routes/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-from . import session
-
-
-bp = Blueprint("main", __name__)
-
-
-bp.register_blueprint(session.session_bp)
--- pre-project-practice/app/routes/session.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-
-
-session_bp = Blueprint("session", __name__, url_prefix="/session")
-
-
-@session_bp.route("/", methods=["GET"])
-def session():
-    pass
