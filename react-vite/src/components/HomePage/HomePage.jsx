28176e05ff3b053dc92a158a0f44c1a6fcdef67a
Mengxuan Liang <mengxuanliang007@gmail.com>
2024-08-13 11:43:26 -0400

    added homepage component

---
+++ react-vite/src/components/HomePage/HomePage.jsx
@@ -0,0 +1,307 @@
+import './HomePage.css'
+
+function HomePage() {
+    return (
+        <div className='main-home'>
+            <div className="container-stroy-home">
+                <div className="left-img-home">
+                    <img src="https://res.cloudinary.com/dhukvbcqm/image/upload/v1723505751/b39ec0_1344b039b28c44d7a55449f3c83d4b41_mv2_vgm2kk.webp" />
+                </div>
+                <div className="container-right-img-home">
+                    <img src="https://res.cloudinary.com/dhukvbcqm/image/upload/v1723505751/b39ec0_1344b039b28c44d7a55449f3c83d4b41_mv2_vgm2kk.webp" />
+                    <img src="https://res.cloudinary.com/dhukvbcqm/image/upload/v1723505751/b39ec0_1344b039b28c44d7a55449f3c83d4b41_mv2_vgm2kk.webp" />
+                    <img src="https://res.cloudinary.com/dhukvbcqm/image/upload/v1723505751/b39ec0_1344b039b28c44d7a55449f3c83d4b41_mv2_vgm2kk.webp" />
+                </div>
+            </div>
+            <div style={{ textAlign: 'center', marginTop: '20px' }}>Fans have paid artists $1.35 billion using Solocamp, and $190 million in the last year.</div>
+            <div className="container-body-home">
+                <div className="container-selling-home">
+                    <h4 style={{ marginLeft: '0%', marginBottom: '20px' }}>SELLING RIGHT NOW</h4>
+                    <div className='container-album-home'>
+                        {/* <div className='container-album-detail-home'>
+                        <img style={{width:'100%'}}src="https://res.cloudinary.com/dhukvbcqm/image/upload/v1723505751/b39ec0_1344b039b28c44d7a55449f3c83d4b41_mv2_vgm2kk.webp"/>
+                        <div>Album title</div>
+                        <div>by artist</div>
+                        <div>Solid for $ </div>
+                    </div> */}
+                        {/* <div className='container-album-detail-home'>
+                        <img style={{width:'100%'}}src="https://res.cloudinary.com/dhukvbcqm/image/upload/v1723505751/b39ec0_1344b039b28c44d7a55449f3c83d4b41_mv2_vgm2kk.webp"/>
+                        <div>Album title</div>
+                        <div>by artist</div>
+                        <div>Solid for $ </div>
+                    </div> */}
+                        <div className='container-album-detail-home'>
+                            <img style={{ width: '100%' }} src="https://res.cloudinary.com/dhukvbcqm/image/upload/v1723505751/b39ec0_1344b039b28c44d7a55449f3c83d4b41_mv2_vgm2kk.webp" />
+                            <div>Album title</div>
+                            <div>by artist</div>
+                            <div>Solid for $ </div>
+                        </div>
+                        <div className='container-album-detail-home'>
+                            <img style={{ width: '100%' }} src="https://res.cloudinary.com/dhukvbcqm/image/upload/v1723505751/b39ec0_1344b039b28c44d7a55449f3c83d4b41_mv2_vgm2kk.webp" />
+                            <div>Album title</div>
+                            <div>by artist</div>
+                            <div>Solid for $ </div>
+                        </div>
+                        <div className='container-album-detail-home'>
+                            <img style={{ width: '100%' }} src="https://res.cloudinary.com/dhukvbcqm/image/upload/v1723505751/b39ec0_1344b039b28c44d7a55449f3c83d4b41_mv2_vgm2kk.webp" />
+                            <div>Album title</div>
+                            <div>by artist</div>
+                            <div>Solid for $ </div>
+                        </div>
+                        <div className='container-album-detail-home'>
+                            <img style={{ width: '100%' }} src="https://res.cloudinary.com/dhukvbcqm/image/upload/v1723505751/b39ec0_1344b039b28c44d7a55449f3c83d4b41_mv2_vgm2kk.webp" />
+                            <div>Album title</div>
+                            <div>by artist</div>
+                            <div>Solid for $ </div>
+                        </div>
+                        <div className='container-album-detail-home'>
+                            <img style={{ width: '100%' }} src="https://res.cloudinary.com/dhukvbcqm/image/upload/v1723505751/b39ec0_1344b039b28c44d7a55449f3c83d4b41_mv2_vgm2kk.webp" />
+                            <div>Album title</div>
+                            <div>by artist</div>
+                            <div>Solid for $ </div>
+                        </div>
+                        <div className='container-album-detail-home'>
+                            <img style={{ width: '100%' }} src="https://res.cloudinary.com/dhukvbcqm/image/upload/v1723505751/b39ec0_1344b039b28c44d7a55449f3c83d4b41_mv2_vgm2kk.webp" />
+                            <div>Album title</div>
+                            <div>by artist</div>
+                            <div>Solid for $ </div>
+                        </div>
+                        <div className='container-album-detail-home'>
+                            <img style={{ width: '100%' }} src="https://res.cloudinary.com/dhukvbcqm/image/upload/v1723505751/b39ec0_1344b039b28c44d7a55449f3c83d4b41_mv2_vgm2kk.webp" />
+                            <div>Album title</div>
+                            <div>by artist</div>
+                            <div>Solid for $ </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="container-events-home">
+                    <h4 style={{ marginLeft: '0%', marginBottom: '20px', paddingTop: '50px' }}>UPCOMING SOLOCAMP LIVE EVENTS</h4>
+                    <div className='container-event-home'>
+                        <div className='container-event-detail-home'>
+                            <img style={{ width: '100%' }} src="https://res.cloudinary.com/dhukvbcqm/image/upload/v1723505751/b39ec0_1344b039b28c44d7a55449f3c83d4b41_mv2_vgm2kk.webp" />
+                            <div>Event name</div>
+                            <div>by group</div>
+                            <br></br>
+                            <div>Listening Party </div>
+                            <div>Date </div>
+                            <div>Time</div>
+                        </div>
+                        <div className='container-event-detail-home'>
+                            <img style={{ width: '100%' }} src="https://res.cloudinary.com/dhukvbcqm/image/upload/v1723505751/b39ec0_1344b039b28c44d7a55449f3c83d4b41_mv2_vgm2kk.webp" />
+                            <div>Event name</div>
+                            <div>by group</div>
+                            <br></br>
+                            <div>Listening Party </div>
+                            <div>Date </div>
+                            <div>Time</div>
+                        </div>
+                        <div className='container-event-detail-home'>
+                            <img style={{ width: '100%' }} src="https://res.cloudinary.com/dhukvbcqm/image/upload/v1723505751/b39ec0_1344b039b28c44d7a55449f3c83d4b41_mv2_vgm2kk.webp" />
+                            <div>Event name</div>
+                            <div>by group</div>
+                            <br></br>
+                            <div>Listening Party </div>
+                            <div>Date </div>
+                            <div>Time</div>
+                        </div>
+                        <div className='container-event-detail-home'>
+                            <img style={{ width: '100%' }} src="https://res.cloudinary.com/dhukvbcqm/image/upload/v1723505751/b39ec0_1344b039b28c44d7a55449f3c83d4b41_mv2_vgm2kk.webp" />
+                            <div>Event name</div>
+                            <div>by group</div>
+                            <br></br>
+                            <div>Listening Party </div>
+                            <div>Date </div>
+                            <div>Time</div>
+                        </div>
+                        <div className='container-event-detail-home'>
+                            <img style={{ width: '100%' }} src="https://res.cloudinary.com/dhukvbcqm/image/upload/v1723505751/b39ec0_1344b039b28c44d7a55449f3c83d4b41_mv2_vgm2kk.webp" />
+                            <div>Event name</div>
+                            <div>by group</div>
+                            <br></br>
+                            <div>Listening Party </div>
+                            <div>Date </div>
+                            <div>Time</div>
+                        </div>
+                        <div className='container-event-detail-home'>
+                            <img style={{ width: '100%' }} src="https://res.cloudinary.com/dhukvbcqm/image/upload/v1723505751/b39ec0_1344b039b28c44d7a55449f3c83d4b41_mv2_vgm2kk.webp" />
+                            <div>Event name</div>
+                            <div>by group</div>
+                            <br></br>
+                            <div>Listening Party </div>
+                            <div>Date </div>
+                            <div>Time</div>
+                        </div>
+                        <div className='container-event-detail-home'>
+                            <img style={{ width: '100%' }} src="https://res.cloudinary.com/dhukvbcqm/image/upload/v1723505751/b39ec0_1344b039b28c44d7a55449f3c83d4b41_mv2_vgm2kk.webp" />
+                            <div>Event name</div>
+                            <div>by group</div>
+                            <br></br>
+                            <div>Listening Party </div>
+                            <div>Date </div>
+                            <div>Time</div>
+                        </div>
+                        <div className='container-event-detail-home'>
+                            <img style={{ width: '100%' }} src="https://res.cloudinary.com/dhukvbcqm/image/upload/v1723505751/b39ec0_1344b039b28c44d7a55449f3c83d4b41_mv2_vgm2kk.webp" />
+                            <div>Event name</div>
+                            <div>by group</div>
+                            <br></br>
+                            <div>Listening Party </div>
+                            <div>Date </div>
+                            <div>Time</div>
+                        </div>
+                        <div className='container-event-detail-home'>
+                            <img style={{ width: '100%' }} src="https://res.cloudinary.com/dhukvbcqm/image/upload/v1723505751/b39ec0_1344b039b28c44d7a55449f3c83d4b41_mv2_vgm2kk.webp" />
+                            <div>Event name</div>
+                            <div>by group</div>
+                            <br></br>
+                            <div>Listening Party </div>
+                            <div>Date </div>
+                            <div>Time</div>
+                        </div>
+                        <div className='container-event-detail-home'>
+                            <img style={{ width: '100%' }} src="https://res.cloudinary.com/dhukvbcqm/image/upload/v1723505751/b39ec0_1344b039b28c44d7a55449f3c83d4b41_mv2_vgm2kk.webp" />
+                            <div>Event name</div>
+                            <div>by group</div>
+                            <br></br>
+                            <div>Listening Party </div>
+                            <div>Date </div>
+                            <div>Time</div>
+                        </div>
+                    </div>
+                    <div className="container-signup-home">
+                        <h4 style={{ textAlign: 'center', marginTop: '70px' }}>Get the best of Bandcamp Daily, delivered every Friday</h4>
+                        <div style={{ textAlign: 'center', marginTop: '20px' }}>
+                            <input placeholder='your email address'></input>
+                            <button style={{ backgroundColor: 'black', color: 'white' }}>SIGN UP</button>
+                        </div>
+                    </div>
+                    <div className="container-discover-home">
+                        <h4>Discover</h4>
+                        <p style={{ color: 'grey' }}>filter by:</p>
+                        <p style={{
+                            backgroundColor: 'lightgrey', borderRadius: '10px', paddingLeft: '10px', paddingRight: '10px', width: 'fit-content',
+                        }}>location</p>
+                        <p style={{ backgroundColor: 'lightgrey', borderRadius: '10px', paddingLeft: '10px', paddingRight: '10px', width: 'fit-content', }}>format</p>
+                        <p style={{ backgroundColor: 'lightgrey', borderRadius: '10px', paddingLeft: '10px', paddingRight: '10px', width: 'fit-content', }}>time</p>
+                    </div>
+                    <div className='container-all-home'>
+                        <p>all</p>
+                        <p>pop</p>
+                        <p>rock</p>
+                        <p>electronic</p>
+                        <p>R&B</p>
+                    </div>
+                    <div className='container-all-home bestselling'>
+                        <p>best-selling</p>
+                        <p>new arrivals</p>
+                        <p>artist-recommended</p>
+                    </div>
+                    <div className='container-all-home artist'>
+                        <p>artists from anywhere</p>
+                        <p>amsterdam</p>
+                        <p>atlanta</p>
+                    </div>
+                    <div className='container-img-list-home'>
+                        <div className='container-two-list-left-home'>
+
+                    
+                        <div className='container-left-img-home'>
+                            <div className='img-info-home'>
+                            <img style={{ width: '100%' }} src="https://res.cloudinary.com/dhukvbcqm/image/upload/v1723505751/b39ec0_1344b039b28c44d7a55449f3c83d4b41_mv2_vgm2kk.webp" />
+                            <div>Album title</div>
+                            <div>Artist</div>
+                            <div>genre</div>
+                            </div>
+                            <div className='img-info-home'>
+                            <img style={{ width: '100%' }} src="https://res.cloudinary.com/dhukvbcqm/image/upload/v1723505751/b39ec0_1344b039b28c44d7a55449f3c83d4b41_mv2_vgm2kk.webp" />
+                            <div>Album title</div>
+                            <div>Artist</div>
+                            <div>genre</div>
+                            </div>
+                            <div className='img-info-home'>
+                            <img style={{ width: '100%' }} src="https://res.cloudinary.com/dhukvbcqm/image/upload/v1723505751/b39ec0_1344b039b28c44d7a55449f3c83d4b41_mv2_vgm2kk.webp" />
+                            <div>Album title</div>
+                            <div>Artist</div>
+                            <div>genre</div>
+                            </div>
+                            <div className='img-info-home'>
+                            <img style={{ width: '100%' }} src="https://res.cloudinary.com/dhukvbcqm/image/upload/v1723505751/b39ec0_1344b039b28c44d7a55449f3c83d4b41_mv2_vgm2kk.webp" />
+                            <div>Album title</div>
+                            <div>Artist</div>
+                            <div>genre</div>
+                            </div>
+                        </div>
+                        <div className='container-left-img-home'>
+                            <div className='img-info-home'>
+                            <img style={{ width: '100%' }} src="https://res.cloudinary.com/dhukvbcqm/image/upload/v1723505751/b39ec0_1344b039b28c44d7a55449f3c83d4b41_mv2_vgm2kk.webp" />
+                            <div>Album title</div>
+                            <div>Artist</div>
+                            <div>genre</div>
+                            </div>
+                            <div className='img-info-home'>
+                            <img style={{ width: '100%' }} src="https://res.cloudinary.com/dhukvbcqm/image/upload/v1723505751/b39ec0_1344b039b28c44d7a55449f3c83d4b41_mv2_vgm2kk.webp" />
+                            <div>Album title</div>
+                            <div>Artist</div>
+                            <div>genre</div>
+                            </div>
+                            <div className='img-info-home'>
+                            <img style={{ width: '100%' }} src="https://res.cloudinary.com/dhukvbcqm/image/upload/v1723505751/b39ec0_1344b039b28c44d7a55449f3c83d4b41_mv2_vgm2kk.webp" />
+                            <div>Album title</div>
+                            <div>Artist</div>
+                            <div>genre</div>
+                            </div>
+                            <div className='img-info-home'>
+                            <img style={{ width: '100%' }} src="https://res.cloudinary.com/dhukvbcqm/image/upload/v1723505751/b39ec0_1344b039b28c44d7a55449f3c83d4b41_mv2_vgm2kk.webp" />
+                            <div>Album title</div>
+                            <div>Artist</div>
+                            <div>genre</div>
+                            </div>
+                        </div>
+                        </div>
+                        <div className='container-right-img-home'>
+                          
+                                <img style={{ width: '100%' }} src="https://res.cloudinary.com/dhukvbcqm/image/upload/v1723505751/b39ec0_1344b039b28c44d7a55449f3c83d4b41_mv2_vgm2kk.webp" />
+                                <div>from the album title</div>
+                                <div>by Artist</div>
+                                <div>Los Angeles, California</div>
+                                <div className='buy-now-home'>
+                                    <div>buy now</div>
+                                    <div>wishlist</div>
+                                    <div>hear more</div>
+                                </div>
+                        </div>
+                    </div>
+                </div>
+                {/* <div className="container-new-notable-home"></div>
+                <div className="container-daily-home"></div> */}
+            </div>
+            <div className="container-footer-home">
+                <div className='containter-footer-detail-home'>
+                    <div className='container-text-home'>
+                        <div style={{ color: 'white' }}>About</div>
+                        <div style={{ color: 'white' }}>Buttons/Logos</div>
+                        <div style={{ color: 'white' }}>Bandcamp Daily</div>
+                        <div style={{ color: 'white' }}>Gift Cards</div>
+                    </div>
+                    <div className='container-text-home'>
+                        <div style={{ color: 'white' }}>About</div>
+                        <div style={{ color: 'white' }}>Buttons/Logos</div>
+                        <div style={{ color: 'white' }}>Bandcamp Daily</div>
+                        <div style={{ color: 'white' }}>Gift Cards</div>
+                    </div>
+                    <div className='container-text-home'>
+                        <div style={{ color: 'white' }}>About</div>
+                        <div style={{ color: 'white' }}>Buttons/Logos</div>
+                        <div style={{ color: 'white' }}>Bandcamp Daily</div>
+                        <div style={{ color: 'white' }}>Gift Cards</div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    )
+
+}
+
+export default HomePage
+
