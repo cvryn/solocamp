4ad50cb223bd05837b1c5768674973b6a7fd8c9e
Mengxuan Liang <mengxuanliang007@gmail.com>
2024-08-18 14:04:40 -0400

    added more styling

--- react-vite/src/components/ManageAlbum/ManageAlbum.jsx
+++ react-vite/src/components/ManageAlbum/ManageAlbum.jsx
@@ -1,15 +1,17 @@
 import { useEffect, useState, useMemo } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { thunkDeleteAlbum, thunkGetAlbums } from "../../redux/albumReducer"
-// import { useParams } from "react-router-dom";
 import './ManageAlbum.css'
 import CreateAlbumButton from "../HomePage/CreateAlbumButton";
 import UpdateAlbumButton from "./UpdateAlbumButton";
+import { useNavigate } from "react-router-dom";
+// import { useParams } from "react-router-dom";
 // import { useLoaderData } from "react-router-dom";
 
 
 
 export default function ManageAlbum() {
+    const navigate = useNavigate()
     const [isLoaded, setIsLoaded] = useState(true);
     const dispatch = useDispatch()
     const userId = useSelector(state => state.session.user?.id)
@@ -32,21 +34,11 @@ export default function ManageAlbum() {
 
     if (!userId) return (<h1 style={{textAlign:'center'}}>Please log in or sign up</h1>)
 
-    // if (!isLoaded ) {
-    //     return <p>Loading...</p>
-    // }
-    // if (!Array.isArray(albumArr)) {
-    //     return <p>Loading...</p>;
-    // }
     const handleDelete = async (id) => {
         await dispatch(thunkDeleteAlbum(id));
         setIsLoaded(false); 
     }
 
-// if (albumArr.length === 0) {
-//     return null;
-// }
-// console.log('ablum array', albumArr)
     if (albumArr && albumArr.map(el => el.album_art) ) {
         return ( isLoaded ?
             <div>Is loading...</div> :
@@ -58,10 +50,10 @@ export default function ManageAlbum() {
                                 <div className="manage-als" key={el.id}>
                                     {/* {el.album_art ? <img style={{ width: '300px' }} src={el.album_art[0].album_art}></img> : <img style={{ width: '300px' }} src="https://res.cloudinary.com/dhukvbcqm/image/upload/v1723505751/b39ec0_1344b039b28c44d7a55449f3c83d4b41_mv2_vgm2kk.webp"></img>} */}
                                     {el?.album_art?.map(art => {
-                                       return <img key={el.id} style={{ width: '100%',height:"30%",marginBottom:'10px' }} src={art.album_art}></img>
+                                       return <img key={el.id} onClick={()=> navigate(`/albums/${el.id}`)} style={{ width: '100%',height:"220px",marginBottom:'10px' }} src={art.album_art}></img>
                                     })}
 
-                                    <div>{el.name}</div>
+                                    <div style={{fontWeight:'bold',fontSize:'20px'}}>{el.name}</div>
                                     <br></br>
                                     <div>{el.year}</div>
 
@@ -70,10 +62,10 @@ export default function ManageAlbum() {
                                     <div>${el.price}</div>
                                     <br></br>
                                     <div>{el.description}</div>
-                                    <div className="update-button-manage-albums">
+                                    <div className="update-button-manage-albums button-hover-effect">
                                         <UpdateAlbumButton el={el} />
                                     </div>
-                                    <button style={{backgroundColor:'lightgray',border:"1px solid black", marginTop:'10px'}}onClick={() => handleDelete(el.id)}>Delete Album</button>
+                                    <button className='delete-button-manage-al button-hover-effect' onClick={() => handleDelete(el.id)}>Delete Album</button>
                                 </div>
                             )
                         })
@@ -90,15 +82,6 @@ export default function ManageAlbum() {
             </div>
         )
     } else {
-        return (
-            null
-            // <div className="create-first-album">
-            //     <h2 style={{textAlign:'center', fontSize:"15px"}}>Post your first album on Solocamp</h2>
-            //     <div className="create-album-button">
-            //     <CreateAlbumButton />
-
-            //     </div>
-            // </div>
-            );
+        return (null);
     }
 }
