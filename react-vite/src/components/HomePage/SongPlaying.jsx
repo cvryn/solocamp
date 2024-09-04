9bb5b8ac2b7222fbf9b3bf88e317e2193aa96458
alan-chang <alanc091322@gmail.com>
2024-09-04 16:38:24 -0700

    pulled latest code

--- react-vite/src/components/HomePage/SongPlaying.jsx
+++ react-vite/src/components/HomePage/SongPlaying.jsx
@@ -2,20 +2,20 @@ import { SiYoutubemusic } from "react-icons/si";
 // import { IoMdHeartEmpty } from "react-icons/io";
 import './HomePage.css'
 
-function SongPlaying({selectedImageId, albumData}) {
-   
+function SongPlaying({ selectedImageId, albumData }) {
+
     // console.log(albumData[selectedImageId])
     return (
         <div className='container-down-right-img-home'>
-            <img style={{ width: '100%' }}  src={albumData[selectedImageId-1]?.album_art[0]?.album_art}/>
+            <img style={{ width: '100%' }} src={albumData[selectedImageId - 1]?.album_art[0]?.album_art} />
             <div className="music-player">
-                <SiYoutubemusic style={{ fontSize: '40px' }} onClick={()=>alert('playing demo feature coming soon...')}/>
+                <SiYoutubemusic style={{ fontSize: '40px' }} onClick={() => alert('playing demo feature coming soon...')} />
                 <span>play a demo</span>
             </div>
             <br></br>
-            <div>from {albumData[selectedImageId-1]?.name}</div>
+            <div>from {albumData[selectedImageId - 1]?.name}</div>
             <br></br>
-            <div>by {albumData[selectedImageId-1]?.user_username}</div>
+            <div>by {albumData[selectedImageId - 1]?.user_username}</div>
             <div>Los Angeles, California</div>
             {/* <div className='buy-now-home'>
                 <div>buy now</div>
@@ -24,7 +24,6 @@ function SongPlaying({selectedImageId, albumData}) {
                     <div>wishlist</div>
                 </div>
             </div> */}
-            
         </div>
 
     )
